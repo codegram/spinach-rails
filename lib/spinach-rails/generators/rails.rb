037fa04d8558cfd291fbb690749fc1fd8987f6d1
Tad Hosford <tad.hosford@gmail.com>
2013-01-07 15:57:04 -0600

    Generated config file includes helper methods for accessing routes for both mounted engines & the app itself

--- lib/spinach-rails/generators/rails.rb
+++ lib/spinach-rails/generators/rails.rb
@@ -9,6 +9,28 @@ require './config/environment'
 
 require 'minitest/spec'
 
+module Spinach
+  class FeatureSteps
+    known_engines = ObjectSpace.enum_for(:each_object, ::Rails::Railtie.singleton_class).to_a
+    engines_with_routes = known_engines.select{ |engine|
+      engine.respond_to?(:routes) && engine.routes.named_routes.routes.present?
+    }
+    engines_with_routes.each do |engine|
+      engine_name = engine.to_s.underscore.split("/")
+      case engine_name.pop
+      when "application"
+        send :define_method, :main_app do
+          engine.routes.url_helpers
+        end
+      when "engine"
+        send :define_method, engine_name[0].to_sym do
+          engine.routes.url_helpers
+        end
+      end
+    end
+  end
+end
+
 # require 'database_cleaner'
 # DatabaseCleaner.strategy = :truncation
 #
